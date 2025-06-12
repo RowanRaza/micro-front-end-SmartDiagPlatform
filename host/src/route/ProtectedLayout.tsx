b14aced51f0784cbf6054e3940f72d8370beed49
RowanRaza <razanajatovo.rowan@gmail.com>
2025-06-12 16:59:01 +0300

    -

--- host/src/route/ProtectedLayout.tsx
+++ host/src/route/ProtectedLayout.tsx
@@ -1,17 +1,18 @@
 import { useState } from 'react';
 import Header from '../component/access-bar/Header';
 import MenuBar from '../component/access-bar/MenuBar';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useNavigate } from 'react-router-dom';
 
 const ProtectedLayout = () => {
     const [sidebarVisible, setSidebarVisible] = useState(true);
+    const navigate = useNavigate();
 
     return (
         <div className="main-container">
             <MenuBar isVisible={sidebarVisible} />
             
             <main className="content">
-                <Header onHamburgerClick={()=>setSidebarVisible(!sidebarVisible)}/>
+                <Header onHamburgerClick={()=>setSidebarVisible(!sidebarVisible)} onProfileClick={() => navigate("mon_compte")}/>
                 <Outlet/>
             </main>
         </div>
